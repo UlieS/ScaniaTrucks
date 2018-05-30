8b92966524b9e6694736bbb7f1ed1c9ce6eb71a6
ulie <ulrike.schnaithmann@gmail.com>
2018-05-30 19:08:52 +0200

    boxplot

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
 from imputer import Imputer
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 df=pd.read_csv(os.getcwd()+"/data/aps_failure_training_set.csv",na_values="na",dtype=str)
 
@@ -12,16 +13,26 @@ df=df.apply(pd.to_numeric)
 # get percentage of NaN per column
 missingValues=df.isnull().sum()/60000
 
+# generate boxplot
+fig, ax = plt.subplots()
+ax.boxplot(np.array(missingValues))
+ax.set_title('Percentage of missing values per feature')
+fig.savefig(os.getcwd()+"/figures/boxplot.png")
+
+# take out features with more than 60% missing values based on visual analysis of boxplot
+colsToDrop=missingValues.where(missingValues >0.6)
+df=df.drop(columns=(colsToDrop[colsToDrop.notnull()].index))
+
 # use kNN imputation to fill in missing values 
-df2=[]
 impute= Imputer()
-col=0
 for col in range(df.shape[1]):
    result= impute.knn(X=df, column=col, k=3)
    df.iloc[:,col]=result[:,col]
-   print(col)
    
-df.tofile('imputed.csv',sep=',',format='%10.5f')
+df.to_csv('training_imputed.csv',sep=',')
 
-
+# potentially take out samples that have more than x amount of unknown values
+missingrows=[]
+for row in range(len(df)):  
+    missingrows.append(df.iloc[row].isnull().sum())
 
