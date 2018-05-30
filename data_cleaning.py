cf832b27dec37ffbc9c51007f1e5447e5de8b9c5
john-kloss <jonathan.kloss1@gmail.com>
2018-05-30 15:42:39 +0200

    imputer

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,18 +3,16 @@ import os
 from imputer import Imputer
 
 
-df=pd.read_csv(os.getcwd()+"/data/aps_failure_training_set.csv",na_values="na",dtype=str)
+df = pd.read_csv(os.getcwd()+"/data/aps_failure_training_set.csv",
+                 na_values="na", dtype=str)
 
-# transform class label to neg=1 pos=0 
-df["class"]=df["class"].apply(lambda x: 1 if x=='neg' else 0)
-df=df.apply(pd.to_numeric)
+# transform class label to neg=1 pos=0
+df["class"] = df["class"].apply(lambda x: 1 if x == 'neg' else 0)
+df = df.apply(pd.to_numeric)
 
 # get percentage of NaN per column
-missingValues=df.isnull().sum()/60000
+missingValues = df.isnull().sum()/60000
 
-# use kNN imputation to fill in missing values 
-impute= Imputer()
+# use kNN imputation to fill in missing values
+impute = Imputer()
 df = impute.knn(X=df, column='ab_000', k=3)
-
-
-
