b942531b1e4ddc3c6bba48bcc373775304f4e3dd
ulie <ulrike.schnaithmann@gmail.com>
2018-06-05 17:01:18 +0200

    added dropping correlated features, restructured code

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,6 +3,44 @@ import os
 from imputer import Imputer
 import matplotlib.pyplot as plt
 import numpy as np
+from operator import itemgetter
+
+
+  
+def merge(corrMatrix,threshold):
+    '''
+    analyzes the correlation between the features and compares whether highly correlated 
+    features correlate highly with the same set of other features
+    returns the features to be merged 
+    
+    input: correlation matrix [pd.DataFrame], threshold of minimum correlation [int] 
+    output: list of features to be merged [list of lists]
+    
+    '''
+    corrMatrix=corrMatrix.where(corrMatrix>threshold) 
+    
+#    corrMatrix.where(corrMatrix>0.95).count().sort_values()   # see how many features correlate highly with another one, 
+                                                                # cd_000 dooes not even correlate with itself?
+#    corrMatrix.where(corrMatrix>0.95).count().sum-len(corrMatrix)   # 258-163=95 features are highly correlated
+
+    highlyCorrelated={}
+    for col in corrMatrix:
+        if (corrMatrix.index[corrMatrix[col].notnull()].size>1):
+            highlyCorrelated[col]=corrMatrix.index[corrMatrix[col].notnull()].tolist()
+   
+    highlyCorrelated=sorted(highlyCorrelated.items(),key=itemgetter(1))
+    result=[]
+    key=1
+    while key<len(highlyCorrelated):
+        toBeMerged=[]
+        toBeMerged.append(highlyCorrelated[key-1][0])
+        while key<len(highlyCorrelated) and highlyCorrelated[key][1]==highlyCorrelated[key-1][1]:
+            toBeMerged.append(highlyCorrelated[key][0])
+            key+=1
+        if len(toBeMerged)>1:
+            result.append(toBeMerged)
+        key+=1
+    return result
 
 
 def clean_data():
@@ -30,21 +68,22 @@ def clean_data():
     colsToDrop=missingValues.where(missingValues >0.6)
     df=df.drop(columns=(colsToDrop[colsToDrop.notnull()].index))
 
+    corrMatrix=df.corr(method='pearson')
+    toBeMerged=merge(corrMatrix, 0.95)
+    
+    # keep the first feature of all sets of highly correlated features and drop the rest
+    list(map(lambda x: x.pop(0),toBeMerged))
+    colsToDrop = [item for sublist in toBeMerged for item in sublist]
+    df=df.drop(columns=colsToDrop)
+        
     
     # potentially take out samples that have more than x amount of unknown values
     #missingrows=[]
     #for row in range(len(df)):  
     #    missingrows.append(df.iloc[row].isnull().sum())
     
-#    corrMatrix.where(corrMatrix>0.95).count().sort_values()   # see how many features correlate highly with another one
-#    corrMatrix.where(corrMatrix>0.95).count().sum-len(corrMatrix)   # 258-163=95 features are highly correlated
-
-    corrMatrix=df.corr(method='pearson')
-    corrMatrix=corrMatrix.where(corrMatrix>0.95) 
-    highlyCorrelated={}
-    for col in corrMatrix:
-        highlyCorrelated[col]=corrMatrix.index[corrMatrix[col].notnull()].tolist()
-   
+    
+    
     # use kNN imputation to fill in missing values
     '''
     impute = Imputer()
@@ -55,6 +94,6 @@ def clean_data():
 
     df.to_csv('training_imputed.csv', sep=',')
     '''
-    return df, highlyCorrelated
+    return df, toBeMerged
 
 df, corr=clean_data()
